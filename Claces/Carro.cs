76057a28dfd9f4349efee4ba0f106279218fa1eb
Edgar <5910usuario@gmail.com>
2024-04-06 11:01:30 -0600

    Agregar archivos de proyecto.

---
+++ Claces/Carro.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case___06_04_2024.Claces
+{
+    internal class Carro
+    {
+        public string Marca {get;}
+        public int Modelo {get;}
+        public string Color {get; set;}
+        public string owner {get; set;}
+
+        private int Encendido = 0;
+        private int velocidad_actual = 0;
+       
+        public int MAXVELOCIDAD {get; set;}
+
+        public Carro(string m, int modelo, string col)
+        {
+            Marca = m;
+            Modelo = modelo;
+            Color = col;
+        }
+
+        public int Encender()
+        {
+            if (Encendido == 0)
+            {
+                Encendido = 1;
+                return Encendido;
+            }
+            else
+            {
+                Encendido = 0;
+                return Encendido;
+            }
+        }
+
+        public int Acelerar()
+        {
+            if (Encendido == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                velocidad_actual += 10;
+                return velocidad_actual;
+            }
+        }
+
+
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+
+
+using Case___06_04_2024.Claces;
+
+Carro cccarrito = new Carro("Toyota", 2024, "Blanco");
+Carro cccarrito2 = new Carro("Nissan",2020,"Azul");
+
+cccarrito.Encender();
+
+
+if (cccarrito.Acelerar() == 0)
+{
+    Console.WriteLine($"El carro está apagado");
+} else
+{
+    Console.WriteLine(cccarrito.Acelerar() + " km/h");
+    Console.WriteLine(cccarrito.Acelerar() + " km/h");
+    Console.WriteLine(cccarrito.Acelerar() + " km/h");
+}
+
+cccarrito.Encender();
+Console.WriteLine(cccarrito.Acelerar() + " km/h");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//cccarrito.Marca = "Toyota";
+//cccarrito.Modelo = 2021;
+cccarrito.Color = "Rojo";
+cccarrito.owner = "Juan";
+cccarrito.MAXVELOCIDAD = 200;
+
+Console.WriteLine($"El carro es un {cccarrito.Marca}");
+
+Console.WriteLine($"Mi segundo carro es un {cccarrito2.Marca}");
+
+Console.ReadKey();
+
+
+//cccarrito.Marca = "Nissan";
+//Console.WriteLine($"El carro es un {cccarrito.Marca}");
+//Console.ReadKey();
+*/
